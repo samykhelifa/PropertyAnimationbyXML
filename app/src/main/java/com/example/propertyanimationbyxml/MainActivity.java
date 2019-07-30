416446df8d810a533463335f93b41d93878f4507
samy <samy.t.khelifa@gmail.com>
2019-07-30 20:07:06 +0200

    initial commit

--- app/src/main/java/com/example/propertyanimationbyxml/MainActivity.java
+++ app/src/main/java/com/example/propertyanimationbyxml/MainActivity.java
@@ -1,14 +1,69 @@
 package com.example.propertyanimationbyxml;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.animation.Animator;
+import android.animation.AnimatorInflater;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView mImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mImage =  findViewById(R.id.image);
+
     }
+
+    public void rotateAnimation(View view) {
+
+        Animator anim = AnimatorInflater
+                .loadAnimator(this, R.animator.rotate);
+        anim.setTarget(mImage);
+        anim.start();
+
+    }
+
+    public void scaleAnimation(View view) {
+
+        Animator anim = AnimatorInflater
+                .loadAnimator(this, R.animator.scale);
+        anim.setTarget(mImage);
+        anim.start();
+
+    }
+
+    public void translateAnimation(View view) {
+
+        Animator anim = AnimatorInflater
+                .loadAnimator(this, R.animator.translate);
+        anim.setTarget(mImage);
+        anim.start();
+
+    }
+
+    public void alphaAnimation(View view) {
+
+        Animator anim = AnimatorInflater
+                .loadAnimator(this, R.animator.alpha);
+        anim.setTarget(mImage);
+        anim.start();
+
+    }
+
+    public void setAnimation(View view) {
+
+        Animator anim = AnimatorInflater
+                .loadAnimator(this, R.animator.set);
+        anim.setTarget(mImage);
+        anim.start();
+
+    }
+
+
 }
